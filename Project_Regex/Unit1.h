6d7b337a95e5d1964bab88f33375d618bf49ae6d
Chee Wai Wong <37726476+moseswong74@users.noreply.github.com>
2022-05-31 20:25:36 +0800

    Add files via upload

---
+++ Project_Regex/Unit1.h
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TEdit *userInput;
+	TLabel *isValidLabel;
+	TButton *validateButton;
+	void __fastcall validateButtonClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
